be7dd8c5727500f3718772d7a08b1664cfac3145
Thiago Cintas <thiago_cintas@outlook.com>
2023-07-20 21:50:11 -0300

    Create Migrations and Connections

--- PesquisaCrypto/Startup.cs
+++ PesquisaCrypto/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PesquisaCrypto.Models;
 
 namespace PesquisaCrypto
 {
@@ -31,7 +33,7 @@ namespace PesquisaCrypto
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            services.AddDbContext<MoedasContexto>(opcoes => opcoes.UseSqlServer(Configuration.GetConnectionString("MoedasConexao")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
